c1a1ef918d2b94606285a3adb2be09302c0700c8
Phong <p7157568@gmail.com>
2016-03-30 18:23:39 +0700

    not change

---
+++ app/src/main/java/dhbk/android/testgooglesearchreturn/BaseActivity.java
@@ -0,0 +1,11 @@
+package dhbk.android.testgooglesearchreturn;
+
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by huynhducthanhphong on 3/30/16.
+ */
+public abstract class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    
+}
