cf5d555ff042a6e3c6e031e2966d83c0d367a451
Phong <p7157568@gmail.com>
2016-04-03 14:12:00 +0700

    feat - toolbar contain 2 edittext

--- app/src/main/java/dhbk/android/testgooglesearchreturn/Activity/DirectionActivity.java
+++ app/src/main/java/dhbk/android/testgooglesearchreturn/Activity/DirectionActivity.java
@@ -1,10 +1,22 @@
 package dhbk.android.testgooglesearchreturn.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
 import dhbk.android.testgooglesearchreturn.R;
 
@@ -12,6 +24,11 @@ import dhbk.android.testgooglesearchreturn.R;
  * Created by huynhducthanhphong on 4/3/16.
  */
 public class DirectionActivity extends BaseActivity{
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
+    private static final String TAG = DirectionActivity.class.getName();
+    private EditText startPoint;
+    private EditText endPoint;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,19 +40,79 @@ public class DirectionActivity extends BaseActivity{
 
         makeMapDefaultSetting();
 
-        EditText startPoint = (EditText)findViewById(R.id.start_point);
-        startPoint.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // call search activity
-            }
-        });
-        EditText endPoint = (EditText)findViewById(R.id.end_point);
+//        startPoint = (EditText)findViewById(R.id.start_point);
+//        startPoint.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                // call search activity
+//                openAutocompleteActivity();
+//            }
+//        });
+        endPoint = (EditText)findViewById(R.id.end_point);
         endPoint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // call search activity
+                openAutocompleteActivity();
             }
         });
     }
+
+    private void openAutocompleteActivity() {
+        try {
+            // The autocomplete activity requires Google Play Services to be available. The intent
+            // builder checks this and throws an exception if it is not the case.
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                    .build(this);
+            startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // Indicates that Google Play Services is either not installed or not up to date. Prompt
+            // the user to correct the issue.
+            GoogleApiAvailability.getInstance().getErrorDialog(this, e.getConnectionStatusCode(),
+                    0 /* requestCode */).show();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // Indicates that Google Play Services is not available and the problem is not easily
+            // resolvable.
+            String message = "Google Play Services is not available: " +
+                    GoogleApiAvailability.getInstance().getErrorString(e.errorCode);
+
+            Log.e(TAG, message);
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Check that the result was from the autocomplete widget.
+        if (requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+            if (resultCode == RESULT_OK) {
+                // Get the user's selected place from the Intent.
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.i(TAG, "Place Selected: " + place.getName());
+
+                // Format the place's details and display them in the TextView.
+//                mPlaceDetailsText.setText(formatPlaceDetails(getResources(), place.getName(),
+//                        place.getId(), place.getAddress(), place.getPhoneNumber(),
+//                        place.getWebsiteUri()));
+//
+//                // Display attributions if required.
+//                CharSequence attributions = place.getAttributions();
+//                if (!TextUtils.isEmpty(attributions)) {
+//                    mPlaceAttribution.setText(Html.fromHtml(attributions.toString()));
+//                } else {
+//                    mPlaceAttribution.setText("");
+//                }
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                Log.e(TAG, "Error: Status = " + status.toString());
+            } else if (resultCode == RESULT_CANCELED) {
+                // Indicates that the activity closed before a selection was made. For example if
+                // the user pressed the back button.
+            }
+        }
+    }
+
+
 }
