71cd606fa140b7b2c23a959861cd8271a31eac10
Thien Nhan <thien.nhan2310@gmail.com>
2016-04-24 13:22:37 +0700

    feat- show saved route on map

--- app/src/main/java/dhbk/android/testgooglesearchreturn/ClassHelp/DBConnection.java
+++ app/src/main/java/dhbk/android/testgooglesearchreturn/ClassHelp/DBConnection.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class DBConnection extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "map";
     public static final String DATABASE_TABLE = "list_trip";
-    public static final int DATABASE_VERSION = 3;
+    public static final int DATABASE_VERSION = 4;
 
     public DBConnection(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
